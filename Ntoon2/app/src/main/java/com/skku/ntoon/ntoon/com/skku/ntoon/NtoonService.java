0b63745b69854d1769e8ba6cb2c9fd863fd943a6
KimHyoJin <lawliet1212@naver.com>
2016-01-06 00:35:08 +0900

    2016.01.06 Ntoon

--- Ntoon2/app/src/main/java/com/skku/ntoon/ntoon/com/skku/ntoon/NtoonService.java
+++ Ntoon2/app/src/main/java/com/skku/ntoon/ntoon/com/skku/ntoon/NtoonService.java
@@ -3,33 +3,38 @@ package com.skku.ntoon.ntoon.com.skku.ntoon;
 import android.app.Activity;
 import android.app.Service;
 import android.content.Intent;
+import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.widget.TextView;
 
 import com.skku.ntoon.ntoon.R;
 
+
 /**
  * Created by kimhyojin on 16. 1. 5..
+ * https://github.com/nickfox/Update-Android-UI-from-a-Service/blob/master/src/com/websmithing/broadcasttest/BroadcastService.java
  */
 public class NtoonService extends Service {
 
     public static final String BROADCAST_ACTION = "com.example.tracking.updateprogress";
     Intent intent;
+    private final Handler handler = new Handler();
+    int counter = 0;
 
     @Override
     public void onCreate() {
 
         intent = new Intent(BROADCAST_ACTION);
         sendBroadcast(intent);
-        DisplayLoggingInfo("안녕하세요! 저는 N툰입니다");
         super.onCreate();
 
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        return super.onStartCommand(intent, flags, startId);
+    public void onStart(Intent intent, int startId) {
+        handler.removeCallbacks(sendUpdatesToUI);
+        handler.postDelayed(sendUpdatesToUI, 1000); // 1 second
     }
 
     @Nullable
@@ -40,12 +45,27 @@ public class NtoonService extends Service {
 
     @Override
     public void onDestroy() {
+        handler.removeCallbacks(sendUpdatesToUI);
         super.onDestroy();
     }
 
-    private void DisplayLoggingInfo(String string) {
+    private Runnable sendUpdatesToUI = new Runnable() {
+        public void run() {
+            DisplayLoggingInfo();
+            handler.postDelayed(this, 2000); // 10 seconds
+        }
+    };
 
-        intent.putExtra("string", string);
+    private void DisplayLoggingInfo() {
+        String a = null;
+        if(counter == 0){
+            a = "안녕하세요! N툰입니다";
+            counter++;
+        }else if(counter == 1){
+            a = "오늘의 추천웹툰은 덴마에요";
+            counter--;
+        }
+        intent.putExtra("string", a);
         sendBroadcast(intent);
     }
 
