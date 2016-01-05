350f970d845c30085f30ecc0dec282e70ff49931
nocno2 <nwcnw2@gmail.com>
2016-01-05 05:06:00 -0800

    16.01.05 namwook

--- Ntoon2/app/src/main/java/com/skku/ntoon/ntoon/MainActivity.java
+++ Ntoon2/app/src/main/java/com/skku/ntoon/ntoon/MainActivity.java
@@ -2,29 +2,17 @@ package com.skku.ntoon.ntoon;
 
 import android.app.Fragment;
 import android.app.FragmentManager;
-import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.view.ViewGroup;
-
 import android.widget.Button;
-import android.widget.TextView;
 
 import com.skku.ntoon.ntoon.finishtoon.FinishFragment;
 import com.skku.ntoon.ntoon.mypage.MypageFragment;
 import com.skku.ntoon.ntoon.store.StoreFragment;
 import com.skku.ntoon.ntoon.webtoon.WebtoonFragment;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
 
         mypageBtn = (Button)findViewById(R.id.mypageBtn_main);
         webtoonBtn = (Button)findViewById(R.id.webtoonBtn_main);
@@ -73,5 +63,4 @@ public class MainActivity extends AppCompatActivity {
             fragmentTransaction.commit();
         }
     };
-
 }
--- Ntoon2/app/src/main/java/com/skku/ntoon/ntoon/webtoon/WebtoonFragment.java
+++ Ntoon2/app/src/main/java/com/skku/ntoon/ntoon/webtoon/WebtoonFragment.java
@@ -2,23 +2,55 @@ package com.skku.ntoon.ntoon.webtoon;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.skku.ntoon.ntoon.R;
 
 /**
  * Created by kimhyojin on 15. 12. 31..
  */
-public class WebtoonFragment extends android.app.Fragment{
-
+public class WebtoonFragment extends Fragment{
+    FragmentPagerAdapter adapterViewPager;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_webtoon, container, false);
+
+        FragmentManager rootFragmentManager = getActivity().getSupportFragmentManager();
+        ViewPager vpPager = (ViewPager) rootView.findViewById(R.id.vpPager);
+        adapterViewPager = new MyPagerAdapter(android.support.v4.app.FragmentManager.getSupportFragmentManager());
+        vpPager.setAdapter(adapterViewPager);
+
+        // Attach the page change listener inside the activity
+        vpPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+
+            // This method will be invoked when a new page becomes selected.
+            @Override
+            public void onPageSelected(int position) {
+                Toast.makeText(getActivity(),
+                        "Selected page position: " + position, Toast.LENGTH_SHORT).show();
+            }
+
+            // This method will be invoked when the current page is scrolled
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                // Code goes here
+            }
+
+            // Called when the scroll state changes:
+            // SCROLL_STATE_IDLE, SCROLL_STATE_DRAGGING, SCROLL_STATE_SETTLING
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                // Code goes here
+            }
+        });
         return rootView;
     }
 }
