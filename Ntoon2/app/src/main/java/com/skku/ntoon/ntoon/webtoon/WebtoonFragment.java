24158c6334c8c2106cec942eb39da6a86f45dcf7
KimHyoJin <lawliet1212@naver.com>
2016-01-06 21:18:27 +0900

    tab

--- Ntoon2/app/src/main/java/com/skku/ntoon/ntoon/webtoon/WebtoonFragment.java
+++ Ntoon2/app/src/main/java/com/skku/ntoon/ntoon/webtoon/WebtoonFragment.java
@@ -2,13 +2,17 @@ package com.skku.ntoon.ntoon.webtoon;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.PagerTabStrip;
+import android.support.v4.view.PagerTitleStrip;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.skku.ntoon.ntoon.R;
@@ -22,14 +26,44 @@ public class WebtoonFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_webtoon, container, false);
+        TabLayout tabLayout = (TabLayout)rootView.findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
 
-        ViewPager vpPager = (ViewPager) rootView.findViewById(R.id.vpPager);
+        final ViewPager viewPager = (ViewPager)rootView.findViewById(R.id.vpPager);
+        //final PagerAdapter adapter = new PagerAdapter
+        //        (getFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(new MyPagerAdapter(getFragmentManager()));
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
 
-        vpPager.setAdapter(new MyPagerAdapter(getFragmentManager()));
-        PagerTabStrip ptStrip = (PagerTabStrip) rootView.findViewById(R.id.pager_header);
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+
+        //ViewPager vpPager = (ViewPager) rootView.findViewById(R.id.vpPager);
+
+        //vpPager.setAdapter(new MyPagerAdapter(getFragmentManager()));
+        //PagerTitleStrip ptStrip = (PagerTitleStrip) rootView.findViewById(R.id.pager_header);
+        //ptStrip.setDistributeEvenly(true);
 
         // Attach the page change listener inside the activity
-        vpPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+        /*vpPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
 
             // This method will be invoked when a new page becomes selected.
             @Override
@@ -50,7 +84,7 @@ public class WebtoonFragment extends Fragment{
             public void onPageScrollStateChanged(int state) {
                 // Code goes here
             }
-        });
+        });*/
         return rootView;
     }
 }
